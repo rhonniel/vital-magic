c5b3cea16645d83df9d9ea92ebed91a26c116afc
rhonniel <rhonniel777@gmail.com>
2025-12-23 10:05:50 -0400

    init project configuration: - Spring boot  - web  - JPA  - actuator - lombok - driver Mysql

---
+++ src/main/java/com/lps/vitalMagic/VitalMagicApplication.java
@@ -0,0 +1,13 @@
+package com.lps.vitalMagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalMagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalMagicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lps/vitalMagic/VitalMagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lps.vitalMagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalMagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
